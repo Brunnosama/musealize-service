b260f22c01f69d00d217bbef6b27e966d7d3a1e7
Brunnosama <apbrunno@gmail.com>
2022-06-28 19:44:22 -0300

    chore: add styled-components

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,18 @@
 import { Button, Container } from "react-bootstrap"
+import styled from "styled-components";
 
 function App() {
   return (
     <Container>
-      <p>Musealize</p>
+      <Title>Musealize</Title>
       <Button>Teste</Button>
     </Container>
 
   );
 }
+
+const Title = styled.p`
+  font-size: 3rem;
+`
 
 export default App;
