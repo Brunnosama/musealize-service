004d17bf5d699514b10fac7d923f6ec036244174
Brunnosama <apbrunno@gmail.com>
2022-07-28 22:59:13 -0300

    feat: install font-awesome react components

--- front-end/src/views/Home/index.tsx
+++ front-end/src/views/Home/index.tsx
@@ -1,3 +1,5 @@
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Container } from "react-bootstrap"
 import { toast } from "react-toastify"
 import styled from "styled-components";
@@ -7,10 +9,11 @@ export function HomeView() {
         <Container>
         <Title>Musealize</Title>
         <Button onClick={() => toast.success('Oi') }>Teste</Button>
+        <FontAwesomeIcon icon={faFacebookSquare} />
       </Container>
     );
 }
 
 const Title = styled.p`
   font-size: 3rem;
-`
+`  
