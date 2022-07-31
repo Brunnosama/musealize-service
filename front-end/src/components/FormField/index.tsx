d3218f3356d20a065eef3819796faf726aeaf831
Brunnosama <apbrunno@gmail.com>
2022-07-31 04:20:36 -0300

    fix: Formfield types fix

--- front-end/src/components/FormField/index.tsx
+++ front-end/src/components/FormField/index.tsx
@@ -1,13 +1,14 @@
+import { InputHTMLAttributes } from "react"
 import { Form, FormControlProps } from "react-bootstrap"
 import { IMaskInput } from "react-imask"
 
-type Props = FormControlProps & {
+type Props = {
     controlId: string
     label?: string
     error?: string
     mask?: { mask: string }[]
     onAccept?: (value: unknown) => void
-}
+} & FormControlProps & InputHTMLAttributes<HTMLInputElement>
 
 export function FormField({ controlId, label, error, mask, onAccept, ...inputProps }: Props) {
     return (
