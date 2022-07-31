baa42fcd7205d6e63f86508bc12cc85ca0e0f3ec
Brunnosama <apbrunno@gmail.com>
2022-07-31 02:29:50 -0300

    feat: install firebase

---
+++ front-end/src/services/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3D9aJKCp93_Hfo6oBPEgDtvbchA8wG7U",
+  authDomain: "musealize-f3255.firebaseapp.com",
+  projectId: "musealize-f3255",
+  storageBucket: "musealize-f3255.appspot.com",
+  messagingSenderId: "1056358756744",
+  appId: "1:1056358756744:web:99d84faf9baee69f100a3d"
+};
+
+const app = initializeApp(firebaseConfig);
