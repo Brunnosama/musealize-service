4dbc4b740d171438d3cd93a50c05c62271e5522e
Brunnosama <apbrunno@gmail.com>
2022-07-31 01:50:14 -0300

    fix: fix footer bottom position

--- front-end/src/assets/css/global.ts
+++ front-end/src/assets/css/global.ts
@@ -4,4 +4,17 @@ export const GlobalStyle = createGlobalStyle`
  body {
     font-family: 'Montserrat', sans-serif;
  }
+
+html, body, #root {
+    height: 100%;
+}
+
+#root {
+    display: flex;
+    flex-direction: column;
+}
+
+#root > main {
+    flex: 1;
+}
 `
