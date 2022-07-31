e94d59105935f072bf34001c547359cb2a52797a
Brunnosama <apbrunno@gmail.com>
2022-07-31 04:09:56 -0300

    feat: controlled inputs with formik

--- front-end/src/views/Register/index.tsx
+++ front-end/src/views/Register/index.tsx
@@ -1,3 +1,4 @@
+import { useFormik } from "formik";
 import { Col, Container, Form, FormGroup, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { CustomButton } from "../../components/CustomButton";
@@ -5,7 +6,33 @@ import { FormField } from "../../components/FormField";
 import { Layout } from "../../components/Layout";
 import { PageTitle } from "../../components/PageTitle";
 
+type FormValues = {
+    companyName: string
+    companyEmail: string
+    companyPhone: string
+    companyPassword: string
+    companyAgree: boolean
+}
+
 export function RegisterView() {
+    const formik = useFormik<FormValues>({
+        initialValues: {
+            companyName: '',
+            companyEmail: '',
+            companyPhone: '', 
+            companyPassword: '',
+            companyAgree: false
+        },
+        onSubmit: (values) => {
+            console.log('oi!', values)
+        }
+    })
+    const getFieldProps = (fieldName: keyof FormValues) => {
+        return {
+            ...formik.getFieldProps(fieldName),
+            controlId: `input-${fieldName}`
+        }
+    }
     return (
         <Layout>
             <Container>
@@ -14,58 +41,63 @@ export function RegisterView() {
                         <PageTitle>
                             Nova Conta
                         </PageTitle>
-                        <Form className='mt-4'>
+                        <Form
+                            className='mt-4'
+                            onSubmit={formik.handleSubmit}>
                             <FormField
                                 label='Nome da Instituição'
                                 placeholder='Digite o nome da Instituição'
-                                controlId='input-company-name'
                                 error='Preencha seu nome.'
-                                // isInvalid
-                                // mask={[
-                                //     { mask: '' }
-                                // ]}
+                                {...getFieldProps('companyName')}
+
+                            // isInvalid
+                            // mask={[
+                            //     { mask: '' }
+                            // ]}
                             />
                             <FormField
                                 type='email'
                                 label='E-mail'
                                 placeholder='O e-mail será seu nome de usuário'
-                                controlId='input-company-email'
                                 error='Preencha um e-mail válido.'
-                                // isInvalid
-                                // mask={[
-                                //     { mask: '' }
-                                // ]}
+                                {...getFieldProps('companyEmail')}
+                            // isInvalid
+                            // mask={[
+                            //     { mask: '' }
+                            // ]}
                             />
                             <FormField
                                 type='tel'
                                 label='Telefone'
                                 placeholder='(00) 0000-0000'
-                                controlId='input-company-phone'
                                 error='Registre um número válido.'
-                                // isInvalid
                                 mask={[
                                     { mask: '(00) 0000-0000' },
                                     { mask: '(00) 00000-0000' }
                                 ]}
+                                {...getFieldProps('companyPhone')}
+                                onAccept={value => formik.setFieldValue('companyPhone', value)}
+                            // isInvalid
                             />
                             <FormField
                                 type='password'
                                 label='Senha'
                                 placeholder='Informe sua senha de acesso'
-                                controlId='input-company-password'
                                 error='Registre um número válido.'
-                                // isInvalid
-                                // mask={[
-                                //     { mask: '' }
-                                // ]}
+                                {...getFieldProps('companyPassword')}
+                            // isInvalid
+                            // mask={[
+                            //     { mask: '' }
+                            // ]}
                             />
                             <FormGroup className='mb-3 mt-4' controlId='input-company-agree'>
                                 <Form.Check
+                                    {...getFieldProps('companyAgree')}
                                     type='checkbox'
                                     label={<>Eu li e aceito os <a href='/termos-de-uso.pdf' target='_blank'>Termos de Uso</a>.</>}
                                 />
                                 <div className='d-grid mb-4 mt-4'>
-                                    <CustomButton>
+                                    <CustomButton type='submit'>
                                         Criar conta
                                     </CustomButton>
                                 </div>
