ccc4ee5c771e1a2553f430c9916a31c173cdac37
Brunnosama <apbrunno@gmail.com>
2022-07-31 01:32:35 -0300

    feat: start implement of register screen

--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes as RDRoutes } from 'react-router-dom';
 import { HomeView } from './views/Home';
-import {NotFoundView} from './views/NotFound'
+import { NotFoundView } from './views/NotFound'
+import { RegisterView } from './views/Register';
 
 export function Routes() {
     return (
         <RDRoutes>
             <Route path='/' element={<HomeView />} />
-            <Route path='*' element={<NotFoundView/>} />
+            <Route path='*' element={<NotFoundView />} />
+            <Route path='/cadastro' element={<RegisterView />} />
         </RDRoutes>
     );
 }
---
+++ front-end/src/views/Register/index.tsx
@@ -0,0 +1,23 @@
+import { Container } from "react-bootstrap";
+import { FormField } from "../../components/FormField";
+import { Layout } from "../../components/Layout";
+import { PageTitle } from "../../components/PageTitle";
+
+export function RegisterView() {
+    return (
+        <Layout>
+            <Container>
+                <PageTitle>
+                    Nova Conta
+                </PageTitle>
+                <FormField
+                    controlId='userName'
+                    label='Nome da Instituição'
+                    placeholder='Digite o nome da Instituição'
+                    error='Preencha seu nome.'
+                    isInvalid
+                />
+            </Container>
+        </Layout>
+    );
+}
